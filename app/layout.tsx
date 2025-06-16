0c6a1b8e1b3bd07752961a3213b4708a59b43193
satishdonerao <doneraosatish@gmail.com>
2025-06-16 17:01:14 +0530

    colur

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="bg-[#0e3c39]">
+    <html lang="en" className="bg-[#f0f0f0]">
       <body className={`${poppins.variable} font-sans antialiased`}>
         {children}
       </body>
